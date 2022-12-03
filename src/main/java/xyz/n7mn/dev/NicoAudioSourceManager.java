552e2ea37203d4df1508d8b207721a1adce9e5c1
tina-chan <kassi_0123@kassi-hp.info>
2022-12-03 14:36:17 +0900

    Initial commit

---
+++ src/main/java/xyz/n7mn/dev/NicoAudioSourceManager.java
@@ -0,0 +1,151 @@
+package xyz.n7mn.dev;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.tools.io.HttpClientTools;
+import com.sedmelluq.discord.lavaplayer.tools.io.HttpConfigurable;
+import com.sedmelluq.discord.lavaplayer.tools.io.HttpInterface;
+import com.sedmelluq.discord.lavaplayer.tools.io.HttpInterfaceManager;
+import com.sedmelluq.discord.lavaplayer.track.AudioItem;
+import com.sedmelluq.discord.lavaplayer.track.AudioReference;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
+import lombok.SneakyThrows;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.sedmelluq.discord.lavaplayer.tools.FriendlyException.Severity.SUSPICIOUS;
+
+public class NicoAudioSourceManager implements AudioSourceManager, HttpConfigurable {
+    private HttpInterfaceManager httpInterfaceManager = HttpClientTools.createDefaultThreadLocalManager();
+    /* ニコニコのsoから始まるリンクはアニメなど・・レガシー。 */
+    private static final String TRACK_URL_REGEX = "^(?:http://|https://|)(?:www\\.|)nicovideo\\.jp/watch/((so|sm)[0-9]+)(?:\\?.*|)$";
+    private static final Pattern trackUrlPattern = Pattern.compile(TRACK_URL_REGEX);
+
+    private static final String MY_LIST_URL_REGEX = "^(?:http://|https://|)(?:www\\.|)nicovideo\\.jp/user/([0-9]+)/mylist/([0-9]+)(?:\\?.*|)$";
+    private static final Pattern myListUrlPattern = Pattern.compile(MY_LIST_URL_REGEX);
+
+    public NicoAudioSourceManager() {
+        System.out.println("registered");
+    }
+
+    @Override
+    public String getSourceName() {
+        return "himu-niconico";
+    }
+
+
+    @Override
+    public AudioItem loadItem(AudioPlayerManager manager, AudioReference reference) {
+        Matcher trackMatcher = trackUrlPattern.matcher(reference.identifier);
+
+        if (trackMatcher.matches()) {
+            return loadTrack(trackMatcher.group(1));
+        }/* else {
+            Matcher myListMatcher = myListUrlPattern.matcher(reference.identifier);
+            if (myListMatcher.matches()) {
+                loadMyList(reference.identifier);
+            }
+        }*/
+
+        return null;
+    }
+
+    public AudioTrack loadMyList(String link) {
+        try (HttpInterface httpInterface = getHttpInterface()) {
+            try (CloseableHttpResponse response = checkStatusCode(httpInterface.execute(new HttpGet(link)))) {
+                Document document = Jsoup.parse(response.getEntity().getContent(), StandardCharsets.UTF_8.name(), "", Parser.xmlParser());
+
+                //List<AudioTrack> tracks = getAllTrack(document.getElementById("ContinuousPlayButton MylistMenu-continuous").attr("href"));
+
+                System.out.println(document.getElementById("js-initial-userpage-data").attr("data-environment"));
+            }
+        } catch (IOException e) {
+            throw new FriendlyException("Error occurred when extracting video info.", SUSPICIOUS, e);
+        }
+        return null;
+    }
+
+    public List<AudioTrack> getAllTrack(String url) {
+        return null;
+    }
+
+    public AudioTrack loadTrack(String pattern) {
+        try (HttpInterface httpInterface = getHttpInterface()){
+            try (CloseableHttpResponse response = checkStatusCode(httpInterface.execute(new HttpGet(getVideoUrl(pattern))))) {
+                return loadTrack(pattern, Jsoup.parse(response.getEntity().getContent(), StandardCharsets.UTF_8.name(), "", Parser.xmlParser()));
+            }
+        } catch (IOException e) {
+            throw new FriendlyException("Error occurred when extracting video info.", SUSPICIOUS, e);
+        }
+    }
+
+    public AudioTrack loadTrack(String pattern, Document document) {
+        JSONObject object = new JSONObject(document.getElementById("js-initial-watch-data").attr("data-api-data"));
+
+        return new NicoAudioTrack(new AudioTrackInfo(object.getJSONObject("video").getString("title"), object.getJSONObject("owner").getString("nickname"), object.getJSONObject("video").getLong("duration") * 1000, pattern, false, getVideoUrl(pattern)), this);
+    }
+
+    public String getVideoUrl(String pattern) {
+        return "https://www.nicovideo.jp/watch/" + pattern;
+    }
+
+    @SneakyThrows
+    public static CloseableHttpResponse checkStatusCode(CloseableHttpResponse response) throws IOException {
+        if (!HttpClientTools.isSuccessWithContent(response.getStatusLine().getStatusCode())) {
+            throw new IOException("Unexpected response code from video info: " + response.getStatusLine().getStatusCode());
+        }
+        return response;
+    }
+
+    @Override
+    public boolean isTrackEncodable(AudioTrack track) {
+        return true;
+    }
+
+    @Override
+    public void encodeTrack(AudioTrack track, DataOutput output) throws IOException {
+
+    }
+
+    @Override
+    public AudioTrack decodeTrack(AudioTrackInfo trackInfo, DataInput input) throws IOException {
+        return new NicoAudioTrack(trackInfo, this);
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    public HttpInterface getHttpInterface() {
+        return httpInterfaceManager.getInterface();
+    }
+
+    @Override
+    public void configureRequests(Function<RequestConfig, RequestConfig> configurator) {
+        httpInterfaceManager.configureRequests(configurator);
+    }
+
+    @Override
+    public void configureBuilder(Consumer<HttpClientBuilder> configurator) {
+        httpInterfaceManager.configureBuilder(configurator);
+    }
+}
---
+++ src/main/java/xyz/n7mn/dev/NicoAudioTrack.java
@@ -0,0 +1,178 @@
+package xyz.n7mn.dev;
+
+import com.sedmelluq.discord.lavaplayer.container.mpeg.MpegAudioTrack;
+import com.sedmelluq.discord.lavaplayer.tools.io.HttpInterface;
+import com.sedmelluq.discord.lavaplayer.tools.io.PersistentHttpStream;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
+import com.sedmelluq.discord.lavaplayer.track.DelegatedAudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.playback.LocalAudioTrackExecutor;
+import lombok.SneakyThrows;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpOptions;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
+public class NicoAudioTrack extends DelegatedAudioTrack {
+    private static final Logger log = LoggerFactory.getLogger(NicoAudioTrack.class);
+    protected final NicoAudioSourceManager sourceManager;
+
+    private String data, sessionId;
+    private boolean running;
+
+    /**
+     * @param trackInfo Track info
+     */
+    public NicoAudioTrack(AudioTrackInfo trackInfo, NicoAudioSourceManager sourceManager) {
+        super(trackInfo);
+        this.sourceManager = sourceManager;
+    }
+
+    @Override
+    public void process(LocalAudioTrackExecutor executor) throws Exception {
+        try (HttpInterface httpInterface = sourceManager.getHttpInterface()) {
+            String playbackUrl = loadUrl(httpInterface);
+            log.debug("Starting KeepAlive from SessionId: {}", sessionId);
+            startKeepAlive();
+            log.debug("Starting NicoNico track from URL: {}", playbackUrl);
+            try (PersistentHttpStream stream = new PersistentHttpStream(httpInterface, new URI(playbackUrl), null)) {
+                processDelegate(new MpegAudioTrack(trackInfo, stream), executor);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    public void startKeepAlive() {
+        running = true;
+        new Thread(() -> {
+            try {
+                while (running) {
+                    Thread.sleep(40000L);
+                    //動画のリンクを維持するために実行します！
+                    executeKeepAlive();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                stopKeepAlive();
+            }
+        }).start();
+    }
+
+    public void executeKeepAlive() throws Exception {
+        try (HttpInterface httpInterface = sourceManager.getHttpInterface())  {
+            checkStatusCode(httpInterface.execute(new HttpOptions("https://api.dmc.nico/api/sessions/" + sessionId + "?_format=json&_method=PUT")));
+
+            HttpPost post = new HttpPost("https://api.dmc.nico/api/sessions/" + sessionId + "?_format=json&_method=PUT");
+            post.setEntity(new StringEntity(data));
+            post.setHeader("Accept", "application/json");
+            post.setHeader("Content-type", "application/json");
+            post.setHeader("User-Agent", "NicoUtils/1.0 (https://github.com/KoutaChan/NicoUtils)");
+            //lets go
+            data = new JSONObject(EntityUtils.toString(checkStatusCode(httpInterface.execute(post)).getEntity(), StandardCharsets.UTF_8.name())).getJSONObject("data").toString();
+        }
+    }
+
+    public void stopKeepAlive() {
+        if (running) {
+            running = false;
+        }
+    }
+
+    public String loadUrl(HttpInterface httpInterface) throws IOException {
+        CloseableHttpResponse response = checkStatusCode(httpInterface.execute(new HttpGet(trackInfo.uri)));
+
+        String supporting = createJson(Jsoup.parse(response.getEntity().getContent(), StandardCharsets.UTF_8.name(), "", Parser.xmlParser()));
+
+        HttpPost post = new HttpPost("https://api.dmc.nico/api/sessions?_format=json");
+        post.setEntity(new StringEntity(supporting));
+        post.setHeader("Accept", "application/json");
+        post.setHeader("Content-type", "application/json");
+        post.setHeader("User-Agent", "NicoUtils/1.0 (https://github.com/KoutaChan/NicoUtils)");
+
+        JSONObject dataJson = new JSONObject(EntityUtils.toString(checkStatusCode(httpInterface.execute(post)).getEntity(), StandardCharsets.UTF_8.name())).getJSONObject("data");
+        this.data = dataJson.toString();
+
+        JSONObject session = dataJson.getJSONObject("session");
+
+        this.sessionId = session.getString("id");
+        return session.getString("content_uri");
+    }
+
+    public String createJson(Document document) {
+        JSONObject object = new JSONObject(document.getElementById("js-initial-watch-data").attr("data-api-data"));
+
+        final JSONObject session = object.getJSONObject("media")
+                .getJSONObject("delivery")
+                .getJSONObject("movie")
+                .getJSONObject("session");
+        final JSONObject urls = session.getJSONArray("urls").getJSONObject(0);
+
+        final JSONObject sessions = new JSONObject()
+                .put("recipe_id", session.getString("recipeId"))
+                .put("content_id", session.getString("contentId"))
+                .put("priority", session.getInt("priority"))
+                .put("content_type", "movie")
+                .append("content_src_id_sets", new JSONObject().append("content_src_ids",  new JSONObject().put("src_id_to_mux", new JSONObject()
+                        .append("video_src_ids", session.getJSONArray("videos").getString(0))
+                        .append("audio_src_ids", session.getJSONArray("audios").getString(0)))))
+                .put("timing_constraint", "unlimited")
+                .put("keep_method", new JSONObject().put("heartbeat", new JSONObject().put("lifetime", session.getInt("heartbeatLifetime"))))
+                .put("client_info", new JSONObject().put("player_id", session.getString("playerId")))
+                .put("content_uri", "")
+                .put("session_operation_auth", new JSONObject().put("session_operation_auth_by_signature", new JSONObject()
+                        .putOnce("token", session.getString("token"))
+                        .put("signature", session.getString("signature"))))
+                .put("protocol", new JSONObject()
+                        .put("name", "http")
+                        .put("parameters", new JSONObject().put("http_parameters", new JSONObject().put("parameters", new JSONObject().put("http_output_download_parameters", new JSONObject()
+                                .put("use_well_known_port", urls.getBoolean("isWellKnownPort") ? "yes" : "no")
+                                .put("use_ssl", urls.getBoolean("isSsl") ? "yes" : "no")
+                                .put("transfer_preset", "")
+                                .put("segment_duration", 6000))))))
+                .put("content_auth", new JSONObject()
+                        .put("auth_type", session.getJSONObject("authTypes").getString("http"))
+                        .put("content_key_timeout", session.getInt("contentKeyTimeout"))
+                        .put("service_id", "nicovideo")
+                        .put("service_user_id", session.getString("serviceUserId")));
+        //Once
+        return new JSONObject().put("session", sessions).toString();
+    }
+
+    @SneakyThrows
+    public static CloseableHttpResponse checkStatusCode(CloseableHttpResponse response) throws IOException{
+        int statusCode = response.getStatusLine().getStatusCode();
+
+        if (statusCode == HttpStatus.SC_OK || statusCode == HttpStatus.SC_PARTIAL_CONTENT ||
+                statusCode == HttpStatus.SC_NON_AUTHORITATIVE_INFORMATION || statusCode == HttpStatus.SC_CREATED) {
+            return response;
+        }
+        throw new IOException("Unexpected response code from video info: " + response.getStatusLine().getStatusCode());
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+        System.out.println("stopping");
+        stopKeepAlive();
+    }
+
+    @Override
+    protected AudioTrack makeShallowClone() {
+        return new NicoAudioTrack(trackInfo, sourceManager);
+    }
+}
